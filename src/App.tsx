ad2be6a9b05ab3214ef811a831a13507c59e4639
Alcides Turruellas <ayunierto@gmail.com>
2024-05-17 06:08:09 -0500

    theming setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { Button } from "./components/ui/button";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Button variant={"outline"}>Hello World</Button>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
+      <div className="flex items-center justify-center h-screen">
+        <Button variant={"outline"}>Hello World</Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
