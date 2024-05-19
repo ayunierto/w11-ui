3436eb56548c773785f69aa8c528705b85065bc5
Alcides Turruellas <ayunierto@gmail.com>
2024-05-19 05:40:25 -0500

    created button component

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,83 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <ModeToggle />
-      <div className="flex items-center justify-center h-screen">
-        <Button variant={"outline"}>Hello World</Button>
+      <div className="m-4 flex flex-col gap-5">
+        <h1 className="text-3xl font-medium">Button</h1>
+        <p>
+          A button is a widget that enables users to trigger an action or event,
+          such as submitting a form, opening a dialog, canceling an action, or
+          performing a delete operation.
+        </p>
+
+        <hr className="opacity-20" />
+
+        <div className="flex flex-col gap-2">
+          <h2 className="text-xl font-medium">Default</h2>
+          <div className="border border-[#e5e5e5] rounded-lg p-3 bg-[#fbfbfb] flex gap-2">
+            <Button>default</Button>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h2 className="text-xl font-medium">Colors</h2>
+          <div className="border border-[#e5e5e5] rounded-lg p-3 bg-[#fbfbfb] flex gap-2">
+            <Button colors={"default"}>default</Button>
+            <Button colors={"primary"}>primary</Button>
+            <Button colors={"destructive"}>destructive</Button>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h2 className="text-xl font-medium">Variants</h2>
+          <div className="border border-[#e5e5e5] rounded-lg p-3 bg-[#fbfbfb] flex flex-col gap-2">
+            <div className="flex gap-2">
+              <Button colors={"default"} variant={"flat"}>
+                flat
+              </Button>
+              <Button colors={"default"} variant={"solid"}>
+                solid
+              </Button>
+              <Button colors={"default"} variant={"outline"}>
+                outline
+              </Button>
+            </div>
+            <div className="flex gap-2">
+              <Button colors={"primary"} variant={"flat"}>
+                flat
+              </Button>
+              <Button colors={"primary"} variant={"solid"}>
+                solid
+              </Button>
+              <Button colors={"primary"} variant={"outline"}>
+                outline
+              </Button>
+            </div>
+            <div className="flex gap-2">
+              <Button colors={"destructive"} variant={"flat"}>
+                flat
+              </Button>
+              <Button colors={"destructive"} variant={"solid"}>
+                solid
+              </Button>
+              <Button colors={"destructive"} variant={"outline"}>
+                outline
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 ">
+          <h2 className="text-xl font-medium">Sizes</h2>
+          <div className="border border-[#e5e5e5] rounded-lg p-3 bg-[#fbfbfb] flex gap-2">
+            <Button colors={"primary"} size={"sm"}>
+              sm
+            </Button>
+            <Button colors={"primary"} size={"md"}>
+              mg
+            </Button>
+            <Button colors={"primary"} size={"lg"}>
+              lg
+            </Button>
+          </div>
+        </div>
       </div>
     </ThemeProvider>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,32 +5,33 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
   {
     variants: {
-      variant: {
+      colors: {
         default:
-          "bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
+          "bg-neutral-200 hover:bg-neutral-300 border border-neutral-200 border-b-neutral-300 ",
+        primary:
+          "bg-blue-700 hover:bg-blue-600 hover:text-white border border-blue-700 text-white",
         destructive:
-          "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
-        outline:
-          "border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
-        secondary:
-          "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
-        ghost:
-          "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
-        link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
+          "bg-red-700 hover:bg-red-600 hover:text-white border border-red-700 text-white",
+      },
+      variant: {
+        flat: "bg-transparent border-transparent text-black",
+        solid: "border-transparent",
+        outline: "bg-opacity-0 text-black",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        sm: "h-8 px-3",
+        md: "h-9 px-4 py-2",
+        lg: "h-10 px-8",
+        icon: "h-9 w-9",
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      colors: "default",
+      variant: "solid",
+      size: "md",
     },
   }
 );
@@ -42,11 +43,11 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ colors, className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ colors, variant, size, className }))}
         ref={ref}
         {...props}
       />
@@ -55,4 +56,5 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 Button.displayName = "Button";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export { Button, buttonVariants };
